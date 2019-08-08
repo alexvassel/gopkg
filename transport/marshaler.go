e47e8942923db7b7078b3e1e0895ba12c215030b
sergiusd <s.demchuk@talenttech.ru>
2019-08-08 17:22:57 +0300

    add marshaler option

--- transport/marshaler.go
+++ transport/marshaler.go
@@ -11,15 +11,29 @@ import (
 const contentType = "application/json"
 
 // NewMarshaller ...
-func NewMarshaller() httpruntime.Marshaler {
+func NewMarshaller(options MarshallerOptions) httpruntime.Marshaler {
 	return marshaller{
 		base: httpruntime.MarshalerPbJSON{
-			Marshaler:       &runtime.JSONPb{OrigName: true, EmitDefaults: true, EnumsAsInts: false},
-			Unmarshaler:     &runtime.JSONPb{OrigName: true, EmitDefaults: true, EnumsAsInts: false},
-			GogoMarshaler:   &gogojsonpb.Marshaler{OrigName: true, EmitDefaults: true, EnumsAsInts: false},
+			Marshaler:       &runtime.JSONPb{OrigName: true, EmitDefaults: options.emitDefaults, EnumsAsInts: false},
+			Unmarshaler:     &runtime.JSONPb{OrigName: true, EmitDefaults: options.emitDefaults, EnumsAsInts: false},
+			GogoMarshaler:   &gogojsonpb.Marshaler{OrigName: true, EmitDefaults: options.emitDefaults, EnumsAsInts: false},
 			GogoUnmarshaler: &gogojsonpb.Unmarshaler{AllowUnknownFields: true},
 		},
 	}
+}
+
+func NewMarshallerOptions() MarshallerOptions {
+	return MarshallerOptions{
+		emitDefaults: true,
+	}
+}
+
+func (m MarshallerOptions) WithEmitDefaults(val bool) {
+	m.emitDefaults = val
+}
+
+type MarshallerOptions struct {
+	emitDefaults bool
 }
 
 type marshaller struct {
