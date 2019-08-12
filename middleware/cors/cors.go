e7d1c2eb2d41ad31ba209443c2c72761acfbc631
sergiusd <s.demchuk@talenttech.ru>
2019-08-12 15:50:35 +0300

    add X-Compress in cors

--- middleware/cors/cors.go
+++ middleware/cors/cors.go
@@ -7,7 +7,7 @@ func AddCors(next http.Handler) http.Handler {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
 		w.Header().Add("Access-Control-Allow-Headers",
-			"Accept, X-Token, Content-Type, Content-Length, Accept-Encoding")
+			"Accept, X-Token, X-Compress, Content-Type, Content-Length, Accept-Encoding")
 
 		if r.Method != "OPTIONS" {
 			next.ServeHTTP(w, r)
